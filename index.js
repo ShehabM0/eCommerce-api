66ca7ee4dd12c85cd06469beff345e15cec3a7ca
Shehab <shehabmohamed2357@gmail.com>
2023-02-11 11:22:14 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const express = require("express");
+const bcrypt = require("bcrypt");
+const dotenv = require("dotenv");
+const app = express();
+dotenv.config();
+
+app.use(express.json());
+
+// database connection
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => console.log("Successful DB Connection!"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+try {
+  app.listen(process.env.PORT, () => {
+    console.log(`Listening on ${process.env.PORT}!`);
+  });
+} catch (error) {
+  console.log(error);
+}
