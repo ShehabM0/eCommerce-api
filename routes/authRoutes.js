4f75fe5edd10a6c65956ddbdc3f99cbaeec62305
ShehabM0 <shehabmohamed2357@gmail.com>
2023-02-11 22:33:28 +0200

    added logout route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,9 @@
-const { register, login } = require('../controllers/authentication');
+const { register, logout,  login } = require('../controllers/authentication');
 const express = require('express');
 const router = express.Router();
 
 router.post('/register', register);
+router.get('/logout', logout);
 router.post('/login', login);
 
 module.exports = router;
