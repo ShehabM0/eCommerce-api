33424f824db8eb9b0a0bdbea2f448fdf57f2e412
ShehabM0 <shehabmohamed2357@gmail.com>
2023-02-12 14:25:24 +0200

    added update-userPassword function

--- controllers/user.js
+++ controllers/user.js
@@ -52,7 +52,38 @@ const updateUser = async (req, res) => {
     }
 };
 
+const updateUserPass = async (req, res) => {
+    const {password, newPassword} = req.body;
+    if(!password || !newPassword)
+        return res.status(501).send({ status: "error", message: "Please provide your old password and new password"});
+    try {
+        const findUser = await User.findById(req.params.id);
+        if(!findUser)
+            return res.status(404).send({ status: "error", message: "User not found!"});
+        // checking if entered passwords matches
+        const mathcedPass = await checkPass(password, findUser.password);
+        if(!mathcedPass)
+            return res.status(501).send({ status: "error", message: "Incorrect password!"});
+        // check new password length
+        if (newPassword.length < 6)
+            return res.status(501).send({ status: "error", message: "your new password is too short!" });
+        // hashing new password 
+        const hashPassword = bcrypt.hashSync(newPassword, 10);
+        findUser.password = hashPassword;
+        await findUser.save();
+        // updating user's token
+        const accessToken = createJWT(findUser);
+        JWT_Cookie(res, accessToken, findUser._id);
+
+        res.status(200).send({ status: "ok", user: findUser, token: accessToken });
+    } catch (error) {
+        const err = error.message;
+        res.status(500).send({ status: "error", message: err });
+    }
+}
+
 module.exports = {
+    updateUserPass,
     getAllUsers,
     updateUser,
     getUser
