bc797632af85b3b66e4cae42df4007d07e9c2edb
ShehabM0 <shehabmohamed2357@gmail.com>
2023-02-12 14:38:40 +0200

    added deleteUser route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
-const { updateUserPass, getAllUsers, updateUser,  getUser } = require('../controllers/user');
+const { updateUserPass, getAllUsers, updateUser, deleteUser,  getUser } = require('../controllers/user');
 const express = require('express');
 const router = express.Router();
 
 router.patch('/updatePass/:id', updateUserPass);
+router.delete('/delete/:id', deleteUser);
 router.patch('/update/:id', updateUser);
 router.get('/getAll', getAllUsers);
 router.get('/get/:id', getUser);
