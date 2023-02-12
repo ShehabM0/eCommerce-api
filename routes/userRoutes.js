1e735feff14535291855379c4c1467ff19394aef
ShehabM0 <shehabmohamed2357@gmail.com>
2023-02-12 13:57:36 +0200

    added updateUser route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,8 @@
-const { getAllUsers, getUser } = require('../controllers/user');
+const { getAllUsers, updateUser,  getUser } = require('../controllers/user');
 const express = require('express');
 const router = express.Router();
 
+router.patch('/update/:id', updateUser);
 router.get('/getAll', getAllUsers);
 router.get('/get/:id', getUser);
 
