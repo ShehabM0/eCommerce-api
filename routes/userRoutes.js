60cf5086640f2052ae48a8b401649b3cddef29b5
ShehabM0 <shehabmohamed2357@gmail.com>
2023-02-12 14:24:53 +0200

    added update-userPassword route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,8 @@
-const { getAllUsers, updateUser,  getUser } = require('../controllers/user');
+const { updateUserPass, getAllUsers, updateUser,  getUser } = require('../controllers/user');
 const express = require('express');
 const router = express.Router();
 
+router.patch('/updatePass/:id', updateUserPass);
 router.patch('/update/:id', updateUser);
 router.get('/getAll', getAllUsers);
 router.get('/get/:id', getUser);
