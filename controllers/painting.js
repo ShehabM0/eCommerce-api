faa28e3881940010c28150cf4ff8d88bf2bbc011
ShehabM0 <shehabmohamed2357@gmail.com>
2023-02-13 14:41:26 +0200

    added get(painting(s)) functions

--- controllers/painting.js
+++ controllers/painting.js
@@ -24,7 +24,7 @@ const createPainting = (req, res) => {
                 return res.status(404).send({ status: "error", message: "Please provide image"});
             req.body.image = req.file.path;
             const painting = await Painting.create(req.body);
-            res.status(200).send({ status: "ok",  messagsse: painting });
+            res.status(200).send({ status: "ok",  message: painting });
         } catch (error) {
             const err = error.message;
             res.status(500).send({ status: "error", message: err });
@@ -32,4 +32,36 @@ const createPainting = (req, res) => {
     });
 }
 
-module.exports = createPainting;
+const getPainting = async (req, res) => {
+    let statusCode, messageText;
+    const painting_id = req.params.id;
+    try {
+        const painting = await Painting.findById(painting_id);
+        statusCode = 200;
+        messageText = painting;
+        if(!painting) {
+            statusCode = 400;
+            messageText = "The painting you are looking for doesn't exist!";
+        }
+        res.status(statusCode).send({ status: "ok", message: messageText });    
+    } catch (error) {
+        const err = error.message;
+        res.status(500).send({ status: "error", message: err });
+    }
+}
+
+const getAllPainting = async (req, res) => {
+    try {
+        const paintings = await Painting.find();
+        res.status(200).send({ status: "ok", message: paintings, count: paintings.length });
+    } catch (error) {
+        const err = error.message;
+        res.status(500).sned({ status: "error", message: err });
+    }
+}
+
+module.exports = {
+    createPainting,
+    getAllPainting,
+    getPainting
+};
