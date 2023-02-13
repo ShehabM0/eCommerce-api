362fdf2be5ef02ecb05d32ee4afa0ad92b9d0606
ShehabM0 <shehabmohamed2357@gmail.com>
2023-02-13 14:41:02 +0200

    added get(painting(s)) route

--- routes/paintingRoutes.js
+++ routes/paintingRoutes.js
@@ -1,7 +1,9 @@
-const createPainting = require('../controllers/painting');
+const { createPainting, getPainting, getAllPainting } = require('../controllers/painting');
 const express = require('express');
 const router = express.Router();
 
 router.post("/create" , createPainting);
+router.get("/getAll", getAllPainting);
+router.get("/get/:id", getPainting);
 
 module.exports = router;
