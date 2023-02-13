0df8118d2c48a6b9530c6563d7815e3b64aa8fc8
ShehabM0 <shehabmohamed2357@gmail.com>
2023-02-13 13:31:30 +0200

    added create-painting function

--- controllers/painting.js
+++ controllers/painting.js
@@ -1,8 +1,16 @@
-const imgUpload = require('../helper/imgUpload');
+const formUpload = require('../helper/formUpload');
+const Painting = require('../models/Painting');
 const multer = require('multer');
 
-const addImage = (req, res) => {
-    imgUpload(req, res, function (err) {
+const createPainting = (req, res) => {
+    /*
+        form-input-fields contains Uploaded image file and other
+        Painiting informations, the image validated by multer and the other
+        Painiting informations validated by Painiting schema.
+    */
+    formUpload (req, res, async (err) => 
+    {
+        // handling image errors.
         if (err instanceof multer.MulterError) {
             // A Multer error occurred when uploading.
             return res.status(500).send({ status: "error", message: err.message });
@@ -10,9 +18,18 @@ const addImage = (req, res) => {
             // An unknown error occurred when uploading.
             return res.status(500).send({ status: "error", message: err.message });
         }
-        // no image error for today.
-        res.status(200).send({status: "ok", message: req.file });
-      });
+        // handling painting errors and creating it.
+        try {
+            if(!req.file)
+                return res.status(404).send({ status: "error", message: "Please provide image"});
+            req.body.image = req.file.path;
+            const painting = await Painting.create(req.body);
+            res.status(200).send({ status: "ok",  messagsse: painting });
+        } catch (error) {
+            const err = error.message;
+            res.status(500).send({ status: "error", message: err });
+        }
+    });
 }
 
-module.exports = addImage;
+module.exports = createPainting;
