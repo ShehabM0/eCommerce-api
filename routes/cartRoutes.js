9a24133141a21e9eb7646d1af56dbf6b64b12f2b
ShehabM0 <shehabmohamed2357@gmail.com>
2023-02-14 21:54:58 +0200

    added deleteFromCart route

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,9 +1,10 @@
 const { admin, currUserOradmin } = require("../middleware/authorization");
+const { AddToCart, deleteFromCart } = require("../controllers/cart");
 const verifyToken = require("../middleware/TokenVerification");
-const AddToCart = require("../controllers/cart");
 const express = require("express");
 const router = express.Router();
 
+router.delete("/delete/:id", [verifyToken, currUserOradmin], deleteFromCart);
 router.post("/add/:id", [verifyToken, currUserOradmin], AddToCart);
 
 module.exports = router;
