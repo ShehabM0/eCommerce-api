7cc272731e4a694eff72d943b2c067bfc7e199d2
ShehabM0 <shehabmohamed2357@gmail.com>
2023-02-14 11:24:15 +0200

    added user authorization

---
+++ middleware/authorization.js
@@ -0,0 +1,12 @@
+const admin = (req, res, next) => {
+  if (!req.user.admin)
+    return res.status(403).send({ status: "error", message: "forbidden.." });
+  next();
+};
+
+const currUserOradmin = (req, res, next) => {
+  if (req.user.admin || req.user.id === req.cookies.access_token.id) next();
+  else res.status(403).send({ status: "error", message: "You don't have the permissions!!" });
+};
+
+module.exports = { admin, currUserOradmin };
