8466347a9c44ea64dc3dcb1f6521f92c6eebd406
ShehabM0 <shehabmohamed2357@gmail.com>
2023-02-14 11:26:05 +0200

    assigned middleware To routes

--- routes/paintingRoutes.js
+++ routes/paintingRoutes.js
@@ -1,11 +1,13 @@
 const { createPainting, updatePainting, deletePainting,  getPainting, getAllPainting } = require('../controllers/painting');
+const { admin, currUserOradmin } = require("../middleware/authorization");
+const verifyToken = require("../middleware/TokenVerification");
 const express = require('express');
 const router = express.Router();
 
-router.delete("/delete/:id", deletePainting);
-router.patch("/update/:id", updatePainting);
-router.post("/create" , createPainting);
-router.get("/getAll", getAllPainting);
-router.get("/get/:id", getPainting);
+router.delete("/delete/:id", [verifyToken, admin], deletePainting);
+router.patch("/update/:id", [verifyToken, admin], updatePainting);
+router.post("/create" , [verifyToken, admin], createPainting);
+router.get("/getAll", [verifyToken, admin],  getAllPainting);
+router.get("/get/:id", [verifyToken, admin], getPainting);
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,13 @@
 const { updateUserPass, getAllUsers, updateUser, deleteUser,  getUser } = require('../controllers/user');
+const { admin, currUserOradmin } = require("../middleware/authorization");
+const verifyToken = require("../middleware/TokenVerification");
 const express = require('express');
 const router = express.Router();
 
-router.patch('/updatePass/:id', updateUserPass);
-router.delete('/delete/:id', deleteUser);
-router.patch('/update/:id', updateUser);
-router.get('/getAll', getAllUsers);
-router.get('/get/:id', getUser);
+router.patch('/updatePass/:id', [verifyToken, currUserOradmin], updateUserPass);
+router.patch('/update/:id', [verifyToken, currUserOradmin], updateUser);
+router.delete('/delete/:id', [verifyToken, admin], deleteUser);
+router.get('/getAll', [verifyToken, admin], getAllUsers);
+router.get('/get/:id', [verifyToken, admin], getUser);
 
 module.exports = router;
