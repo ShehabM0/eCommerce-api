377abbc78d0a63420721c3546c545a563ba20814
ShehabM0 <shehabmohamed2357@gmail.com>
2023-02-14 22:12:12 +0200

    added get_user_cart_paintings

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,10 +1,11 @@
 const { admin, currUserOradmin } = require("../middleware/authorization");
-const { AddToCart, deleteFromCart } = require("../controllers/cart");
+const { AddToCart, deleteFromCart, getCart} = require("../controllers/cart");
 const verifyToken = require("../middleware/TokenVerification");
 const express = require("express");
 const router = express.Router();
 
 router.delete("/delete/:id", [verifyToken, currUserOradmin], deleteFromCart);
 router.post("/add/:id", [verifyToken, currUserOradmin], AddToCart);
+router.get("/get", [verifyToken, currUserOradmin], getCart);
 
 module.exports = router;
