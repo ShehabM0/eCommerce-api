216c7b910cfbe35f8fb0c4bc5abd173fe5ce617d
ShehabM0 <shehabmohamed2357@gmail.com>
2023-02-15 08:54:21 +0200

    added get-cart routes

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,11 +1,13 @@
 const { admin, currUserOradmin } = require("../middleware/authorization");
-const { AddToCart, deleteFromCart, getCart} = require("../controllers/cart");
+const { AddToCart, deleteFromCart, getCart, getUserCart, getAllCarts,} = require("../controllers/cart");
 const verifyToken = require("../middleware/TokenVerification");
 const express = require("express");
 const router = express.Router();
 
 router.delete("/delete/:id", [verifyToken, currUserOradmin], deleteFromCart);
 router.post("/add/:id", [verifyToken, currUserOradmin], AddToCart);
-router.get("/get", [verifyToken, currUserOradmin], getCart);
+router.get("/getCart", [verifyToken, currUserOradmin], getCart);
+router.get("/getUserCart", [verifyToken, admin], getUserCart);
+router.get("/getAllCarts", [verifyToken, admin], getAllCarts);
 
 module.exports = router;
